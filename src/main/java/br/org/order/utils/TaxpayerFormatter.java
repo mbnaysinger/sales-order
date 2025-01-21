8b5784c29c024d1f7dd659d4a96b3ad067d3a661
Maike Naysinger Borges <maike.borges@fiergs.org.br>
2025-01-21 19:42:34 -0300

    taxpayer contend

---
+++ src/main/java/br/org/order/utils/TaxpayerFormatter.java
@@ -0,0 +1,44 @@
+package br.org.order.utils;
+
+public class TaxpayerFormatter {
+
+    public static class FormattedTaxpayer {
+        private final long id1;
+        private final int id2;
+        private final int id3;
+
+        public FormattedTaxpayer(long id1, int id2, int id3) {
+            this.id1 = id1;
+            this.id2 = id2;
+            this.id3 = id3;
+        }
+
+        public long getId1() { return id1; }
+        public int getId2() { return id2; }
+        public int getId3() { return id3; }
+    }
+
+    public static FormattedTaxpayer format(String taxpayerId) {
+
+        if (taxpayerId.length() <= 11) {
+            return formatCpf(taxpayerId);
+        } else if (taxpayerId.length() <= 16) {
+            return formatCnpj(taxpayerId);
+        } else {
+            throw new IllegalArgumentException("Formato inválido. CPF deve conter até 11 dígitos. CNPJ deve conter 14 dígitos.");
+        }
+    }
+
+    private static FormattedTaxpayer formatCpf(String cpf) {
+        long id1 = Long.parseLong(cpf.substring(1, 9));
+        int id3 = Integer.parseInt(cpf.substring(9));
+        return new FormattedTaxpayer(id1, 0, id3);
+    }
+
+    private static FormattedTaxpayer formatCnpj(String cnpj) {
+        long id1 = Long.parseLong(cnpj.substring(0, 8));
+        int id2 = Integer.parseInt(cnpj.substring(8, 12));
+        int id3 = Integer.parseInt(cnpj.substring(12));
+        return new FormattedTaxpayer(id1, id2, id3);
+    }
+}
